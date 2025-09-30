521626cd263e837d56560962f889943dc7c507d3
Chaitanya Ponnada <chaitanyaponnada657@gmail.com>
2025-09-30 14:37:18 +0000

    const firebaseConfig = {   apiKey: "AIzaSyA3zCqM0aFSZPI5BG4YsfGgd_bprcyp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 
-import { initializeApp, getApps, getApp } from 'firebase/app';
+import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const getFirebaseApp = () => {
@@ -11,12 +11,13 @@ const getFirebaseApp = () => {
     return getApp();
   }
 
-  const firebaseConfig = {
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  const firebaseConfig: FirebaseOptions = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
   };
 
   if (!firebaseConfig.apiKey) {
