064d2835a5d4c815a434e0b3ab964490568187ae
Chaitanya Ponnada <chaitanyaponnada657@gmail.com>
2025-09-30 14:28:05 +0000

    Admin Sign-in Failed Firebase: Error

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,14 +2,26 @@
 import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+const getFirebaseApp = () => {
+  if (getApps().length > 0) {
+    return getApp();
+  }
+
+  const firebaseConfig = {
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  };
+
+  if (!firebaseConfig.apiKey) {
+    throw new Error('Missing Firebase API Key');
+  }
+
+  return initializeApp(firebaseConfig);
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = getFirebaseApp();
 export const auth = getAuth(app);
