90fe6bdfa967ff028e5256f86b4daf55808ab745
Chaitanya Ponnada <chaitanyaponnada657@gmail.com>
2025-10-01 02:46:09 +0000

    add the light and dark theme toggle button at bottom floating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import { Toaster } from '@/components/ui/toaster';
 import { CartProvider } from '@/hooks/use-cart';
 import { AuthProvider } from '@/hooks/use-auth';
 import { InquiryProvider } from '@/hooks/use-inquiry';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeToggle } from '@/components/theme-toggle';
 
 export const metadata: Metadata = {
   title: 'Project Hub',
@@ -19,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full dark">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -31,15 +33,23 @@ export default function RootLayout({
           'flex min-h-screen flex-col'
         )}
       >
-        <AuthProvider>
-          <InquiryProvider>
-            <CartProvider>
-              <Header />
-              <main className="flex-1">{children}</main>
-              <Toaster />
-            </CartProvider>
-          </InquiryProvider>
-        </AuthProvider>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+          <AuthProvider>
+            <InquiryProvider>
+              <CartProvider>
+                <Header />
+                <main className="flex-1">{children}</main>
+                <Toaster />
+                <ThemeToggle />
+              </CartProvider>
+            </InquiryProvider>
+          </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
