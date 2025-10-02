e538c9fb3fee8e6bfebc73720c445226c714a223
Chaitanya Ponnada <chaitanyaponnada657@gmail.com>
2025-10-02 04:45:00 +0000

    always getting wrong spelling  (_for element h1_)

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -423,7 +423,7 @@ export default function Home() {
                                     <FormItem>
                                         <FormLabel>Phone Number</FormLabel>
                                         <FormControl>
-                                            <Input type="tel" placeholder="+1 (555) 123-4567" {...field} />
+                                            <Input type="tel" placeholder="+91 98765 43210" {...field} />
                                         </FormControl>
                                         <FormMessage />
                                     </FormItem>
@@ -472,4 +472,5 @@ export default function Home() {
   );
 }
 
+    
     
