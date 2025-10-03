33af54f5da487b72d684859b7d3e372f029c9d5c
Chaitanya Ponnada <chaitanyaponnada657@gmail.com>
2025-10-03 07:27:18 +0000

    const firebaseConfig = {   apiKey: "AIzaSyA3zCqM0aFSZPI5BG4YsfGgd_bprcyp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,16 +5,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyA3zCqM0aFSZPI5BG4YsfGgd_bprcypDL0",
+  authDomain: "studio-2375892107-c45c7.firebaseapp.com",
+  projectId: "studio-2375892107-c45c7",
+  storageBucket: "studio-2375892107-c45c7.appspot.com",
+  messagingSenderId: "831626629323",
+  appId: "1:831626629323:web:8edaa747bbe5751fa94b4e"
 };
 
-// Initialize Firebase for SSR
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const db = getFirestore(app);
