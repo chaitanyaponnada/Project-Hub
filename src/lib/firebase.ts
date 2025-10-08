34ea192add658690031e6479782f1046824de00e
Chaitanya Ponnada <chaitanyaponnada657@gmail.com>
2025-10-08 10:20:31 +0000

    const firebaseConfig = {   apiKey: "AIzaSyA3zCqM0aFSZPI5BG4YsfGgd_bprcyp

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,7 +8,7 @@ const firebaseConfig: FirebaseOptions = {
   apiKey: "AIzaSyA3zCqM0aFSZPI5BG4YsfGgd_bprcypDL0",
   authDomain: "studio-2375892107-c45c7.firebaseapp.com",
   projectId: "studio-2375892107-c45c7",
-  storageBucket: "studio-2375892107-c45c7.appspot.com",
+  storageBucket: "studio-2375892107-c45c7.firebasestorage.app",
   messagingSenderId: "831626629323",
   appId: "1:831626629323:web:8edaa747bbe5751fa94b4e"
 };
